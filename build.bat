251cf362a4f8ceb8edd1e06875c54dd23f589900
Michał Janiszewski <janisozaur@gmail.com>
2018-03-03 19:57:17 +0100

    Correct error handling

--- build.bat
+++ build.bat
@@ -3,6 +3,11 @@ pushd vcpkg
 REM Install libraries
 .\vcpkg install curl:%TRIPLET% freetype:%TRIPLET% jansson:%TRIPLET% libpng:%TRIPLET% libzip:%TRIPLET% openssl:%TRIPLET% sdl2:%TRIPLET% speexdsp:%TRIPLET% zlib:%TRIPLET%
 
+if errorlevel 1 (
+    echo failed with %errorlevel%
+    exit /b 0
+)
+
 REM Export libraries
 .\vcpkg export curl:%TRIPLET% freetype:%TRIPLET% jansson:%TRIPLET% libpng:%TRIPLET% libzip:%TRIPLET% openssl:%TRIPLET% sdl2:%TRIPLET% speexdsp:%TRIPLET% zlib:%TRIPLET% --zip --nuget
 

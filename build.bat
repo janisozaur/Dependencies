b2eed10c173140d4a1f97730d40c95ea7fb59301
Michał Janiszewski <janisozaur@users.noreply.github.com>
2018-03-06 16:22:14 +0100

    Update build.bat

--- build.bat
+++ build.bat
@@ -3,9 +3,13 @@ pushd vcpkg
 REM Install libraries
 .\vcpkg install sdl2:%TRIPLET%
 
-.\vcpkg install curl:%TRIPLET% freetype:%TRIPLET% jansson:%TRIPLET% libpng:%TRIPLET% libzip:%TRIPLET% speexdsp:%TRIPLET% zlib:%TRIPLET%
+type C:\projects\dependencies\vcpkg\buildtrees\sdl2\install-arm-windows-dbg-out.log
+
+appveyor PushArtifact C:\projects\dependencies\vcpkg\buildtrees\sdl2\install-arm-windows-dbg-out.log
+
+#.\vcpkg install curl:%TRIPLET% freetype:%TRIPLET% jansson:%TRIPLET% libpng:%TRIPLET% libzip:%TRIPLET% speexdsp:%TRIPLET% zlib:%TRIPLET%
 
 REM Export libraries
-.\vcpkg export curl:%TRIPLET% freetype:%TRIPLET% jansson:%TRIPLET% libpng:%TRIPLET% libzip:%TRIPLET% sdl2:%TRIPLET% speexdsp:%TRIPLET% zlib:%TRIPLET% --zip --nuget
+#.\vcpkg export curl:%TRIPLET% freetype:%TRIPLET% jansson:%TRIPLET% libpng:%TRIPLET% libzip:%TRIPLET% sdl2:%TRIPLET% speexdsp:%TRIPLET% zlib:%TRIPLET% --zip --nuget
 
 popd

285399e0419c36a1e61b1cefb87a58bd1b42f837
Michał Janiszewski <janisozaur@users.noreply.github.com>
2018-03-06 16:09:54 +0100

    Update build.bat

--- build.bat
+++ build.bat
@@ -1,7 +1,9 @@
 pushd vcpkg
 
 REM Install libraries
-.\vcpkg install curl:%TRIPLET% freetype:%TRIPLET% jansson:%TRIPLET% libpng:%TRIPLET% libzip:%TRIPLET% sdl2:%TRIPLET% speexdsp:%TRIPLET% zlib:%TRIPLET%
+.\vcpkg install sdl2:%TRIPLET%
+
+.\vcpkg install curl:%TRIPLET% freetype:%TRIPLET% jansson:%TRIPLET% libpng:%TRIPLET% libzip:%TRIPLET% speexdsp:%TRIPLET% zlib:%TRIPLET%
 
 REM Export libraries
 .\vcpkg export curl:%TRIPLET% freetype:%TRIPLET% jansson:%TRIPLET% libpng:%TRIPLET% libzip:%TRIPLET% sdl2:%TRIPLET% speexdsp:%TRIPLET% zlib:%TRIPLET% --zip --nuget

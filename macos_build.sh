8ab0bf15442c82b046e70d9a2c1c7309c413f994
Michał Janiszewski <janisozaur+signed@gmail.com>
2024-04-08 23:01:40 +0200

    Fix libicu rpath as well

--- macos_build.sh
+++ macos_build.sh
@@ -9,11 +9,11 @@ for lib in x64-osx-openrct2/lib/*.dylib; do
       lib_filename=$(basename "$lib")
       lib_name=$(echo $lib_filename | cut -d'.' -f 1)
       echo "Creating universal (fat) $lib_name"
-      if [ "$lib_name" = "libzip" ]; then
+      if [[ "$lib_name" = "libzip" || $lib_name = libicu* ]]; then
         # libzip embeds the full rpath in LC_RPATH
         # they will be different for arm64 and x86_64
         # this will cause issues, and is unnecessary
-        echo Fixing libzip rpath
+        echo Fixing $lib_name rpath
         install_name_tool -delete_rpath `pwd`"/vcpkg/packages/${lib_name}_x64-osx-openrct2/lib" "x64-osx-openrct2/lib/$lib_filename"
         install_name_tool -delete_rpath `pwd`"/vcpkg/installed/x64-osx-openrct2/x64-osx-openrct2/lib" "x64-osx-openrct2/lib/$lib_filename"
         install_name_tool -delete_rpath `pwd`"/vcpkg/packages/${lib_name}_arm64-osx-openrct2/lib" "arm64-osx-openrct2/lib/$lib_filename"
@@ -47,4 +47,5 @@ done
 (
   cd universal-osx-openrct2 &&
   zip -rXy ../openrct2-libs-v${version}-universal-macos-dylibs.zip * -x '*/.*'
+  sha1sum ../openrct2-libs-v${version}-universal-macos-dylibs.zip
 )
